8ebdde228d648e4ddc9da524d44a533305f76576
Valentin Viennot <valentin.viennot@epfl.ch>
2019-11-13 23:15:08 +0100

    init kittynder code reuse

---
+++ frontend/src/views/Swipe.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="swipe-view">
+    <swipeable-cards />
+  </div>
+</template>
+
+<script>
+import SwipeableCards from "@/components/SwipeableCards.vue";
+
+export default {
+  name: "swipe",
+  components: {
+    SwipeableCards
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  z-index: 0;
+}
+div.swipe-view {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
