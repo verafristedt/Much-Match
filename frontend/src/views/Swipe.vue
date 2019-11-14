7b7b7a421393812978cf29c06bec6939a9d5ace5
Valentin Viennot <valentin.viennot@epfl.ch>
2019-11-14 16:22:54 +0100

    shuffle images array

--- frontend/src/views/Swipe.vue
+++ frontend/src/views/Swipe.vue
@@ -22,9 +22,26 @@ export default {
     };
   },
   mounted() {
+    let shuffle = array => {
+      let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    };
     axios
       .get(`${process.env.VUE_APP_API_BASE}/images`)
-      .then(response => (this.cards = response.data));
+      .then(response => (this.cards = shuffle(response.data)));
   }
 };
 </script>
