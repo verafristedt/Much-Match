b615499265e7b9d5c15951ea461fe324969da05b
Valentin Viennot <valentin.viennot@epfl.ch>
2019-11-14 19:19:36 +0100

    install vuetify ui framework

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false;
 
@@ -10,5 +13,6 @@ Vue.use(VueAxios, axios);
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
