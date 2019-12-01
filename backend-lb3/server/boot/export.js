a8e47822cf0afe475bb1022dd676b68b1c9c366a
Valentin Viennot <valentin.viennot@epfl.ch>
2019-12-01 14:59:29 +0100

    export csv with db delay

--- backend-lb3/server/boot/export.js
+++ backend-lb3/server/boot/export.js
@@ -1,17 +1,40 @@
-const { parse } = require("json2csv");
+const {
+  parse
+} = require("json2csv");
 
-module.exports = function(app) {
+function delay(t, v) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve.bind(null, v), t)
+  });
+}
+
+module.exports = function (app) {
   const Image = app.models.Image;
   const Swipe = app.models.Swipe;
   const Subject = app.models.Subject;
 
-  Swipe.export = function(cb) {
-    Promise.all([Swipe.find(), Image.find(), Subject.find()])
-      .then(([swipes, images, users]) => {
+  Swipe.export = function (db_delay, cb) {
+    db_delay = db_delay ? db_delay : 1200;
+    const swipes = [];
+    const images = [];
+    const users = [];
+    Swipe.find()
+      .then(data => swipes.push(data))
+      .then(() => delay(db_delay))
+      .then(() => Image.find())
+      .then(data => images.push(data))
+      .then(() => delay(db_delay))
+      .then(() => Subject.find())
+      .then(data => users.push(data))
+      .then(() => {
+        const rst = o => JSON.parse(JSON.stringify(o));
+        const addPropsPrefixed = (dst, prefix, src) => {
+          for (const prop in src) {
+            dst[prefix + "_" + prop] = src[prop];
+          }
+        };
         const res = [];
-
         swipes.forEach(swipe => {
-          const rst = o => JSON.parse(JSON.stringify(o));
           swipe = rst(swipe);
           const image = rst(images.find(img => img.id == swipe.image));
           const user = rst(users.find(usr => usr.id == swipe.user));
@@ -20,31 +43,29 @@ module.exports = function(app) {
             img_id: image.id,
             usr_id: user.id
           };
-          const addPropsPrefixed = (dst, prefix, src) => {
-            for (const prop in src) {
-              dst[prefix + "_" + prop] = src[prop];
-            }
-          };
           addPropsPrefixed(swp, "swp", swipe);
           addPropsPrefixed(swp, "usr", user);
           addPropsPrefixed(swp, "img", image);
           res.push(swp);
         });
 
-        // const fields = Object.keys(res[0]);
-        // const opts = { fields };
-
         cb(null, parse(res), "text/csv");
       })
-      .catch(err => cb(err));
+      .catch(err => {
+        console.log(err);
+        cb(err);
+      });
   };
   Swipe.remoteMethod("export", {
     http: {
       path: "/export",
       verb: "get"
     },
-    returns: [
-      {
+    accepts: {
+      arg: 'db_delay',
+      type: 'number'
+    },
+    returns: [{
         arg: "body",
         type: "file",
         root: true
@@ -57,8 +78,5 @@ module.exports = function(app) {
         }
       }
     ]
-    // {
-    //   type: "array"
-    // }
   });
 };
